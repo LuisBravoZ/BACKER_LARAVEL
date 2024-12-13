b245f8b77a3f59dcd8f40a8e24236676cb384e89
LuisBravoZ <luisbravo100715@hotmail.com>
2024-12-13 18:38:46 -0500

    new 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import './bootstrap';   // si estás usando Laravel's Bootstrap
+import { createApp } from 'vue';  // Solo si estás usando Vue
+
+// Crear una instancia de Vue, si estás usando Vue
+const app = createApp({});
+app.mount('#app');
